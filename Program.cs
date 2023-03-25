803b230f1cf85a88874a6937516547063481e9c5
Andrey V <evi696@mail.ru>
2023-03-25 20:42:49 +0500

    methods added

--- Program.cs
+++ Program.cs
@@ -4,3 +4,50 @@
 
 string[] stringArrey = new string[] { "abc1", "bce", "ce222", "ed", "0dfg" };
 int symbols = 3;
+
+int ScanArray(string[] stringArrey, int symbols)
+{
+    int count = 0;
+    for (int i = 0; i < stringArrey.Length; i++)
+    {
+        if (stringArrey[i].Length <= symbols)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] NewArrey(int count)
+{
+    string[] newArrey = new string[count];
+    return newArrey;
+}
+
+
+string[] FillArrey(string[] stringArrey, string[] newArrey, int symbols)
+{
+    int j = 0;
+    for (int i = 0; i < stringArrey.Length; i++)
+    {
+        if (stringArrey[i].Length <= symbols)
+        {
+            newArrey[j] = stringArrey[i];
+            j++;
+        }
+    }
+    return newArrey;
+}
+
+void PrintArrey(string[] arrey)
+{
+    System.Console.WriteLine();
+    System.Console.Write("[");
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        System.Console.Write($" {arrey[i]} ");
+    }
+    System.Console.Write("]");
+}
+
+string[] arrey = NewArrey(ScanArray(stringArrey, symbols));
